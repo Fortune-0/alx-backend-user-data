2407c6284329143c3d9a649601b980d038c547e2
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-09 17:09:23 +0100

    auth file

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Authentication class file"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Placeholder method for authentication logic. """
+        return False
+
+
+def authorization_header(self, request=None) -> str:
+    """"""
+    return None
+
+
+def current_user(self, request=None) -> TypeVar('User'):
+    """"""
+    return None
