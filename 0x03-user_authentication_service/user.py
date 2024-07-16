9c29bb6e2af41d74fcbae014beda3447b588bb2d
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-16 12:36:41 +0100

    documentation

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,6 +10,11 @@ Base = declarative_base()
 
 
 class User(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String)
