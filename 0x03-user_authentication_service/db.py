a4bb8245930d58075339f5a46fcebb36e3d3caa5
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-17 06:19:02 +0100

    - find user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -41,3 +43,19 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by filtering with
+        arbitrary keyword arguments
+
+        Returns:
+            User: _description_
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user:
+                return user
+            else:
+                raise NoResultFound("no user found.")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments.")
