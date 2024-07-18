8390bc0d863de230ed815ab7382fb693a17552d3
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-18 06:07:52 +0100

    Refactored authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,6 +27,30 @@ def _hash_password(password: str) -> bytes:
     return hashed_pass
 
 
+def _register_user(self, email: str, password: str) -> Union[None, User]:
+    """Registers a new user with the given email and password.
+
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            User: The newly created User object.
+
+        Raises:
+            ValueError: If a user with the given email already exists.
+        """
+    try:
+        self.db.find_user_by(email=email)
+        raise ValueError(f"User with {email} alreay exists")
+    except NoResultFound:
+        hashed_password = _hash_password(password)
+        user = User(email=email, hashed_password=hashed_password)
+        self._db.add_user(user)
+        return user
+    pass
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
