c2ebfc94e44d76c9794b026456594dd4cc60669b
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-18 16:35:15 +0100

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -13,9 +13,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index() -> str:
-    """_summary_
+    """ Home Route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['post'])
+def users():
+    """Register a new user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # if not email or password:
+    # #return jsonify({"message": "Email and password required"}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "User created"}), 201
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
