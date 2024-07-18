91f91814e56c3140c4ca006f8b20eb328c7c13b1
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-18 04:39:36 +0100

    - convert password to byte

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+
+
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
+
+from typing import Union
+
+
+def hash_password(self, password: str) -> bytes:
+    """Hashes a password using bcrypt.
+
+        Args:
+            password (str): The password to hash.
+
+        Returns:
+            bytes: The salted hash of the password.
+        """
+    passwrd = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_pass = bcrypt.hashpw(passwrd, salt)
+    return hashed_pass
