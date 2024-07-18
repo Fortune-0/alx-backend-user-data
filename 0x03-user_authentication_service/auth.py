e1e63944bb31e95ed491a8dce4828ac5b74682cd
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-18 06:21:22 +0100

    fixing errores

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,8 +27,17 @@ def _hash_password(password: str) -> bytes:
     return hashed_pass
 
 
-def _register_user(self, email: str, password: str) -> Union[None, User]:
-    """Registers a new user with the given email and password.
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """_summary_
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> Union[None, User]:
+        """Registers a new user with the given email and password.
 
         Args:
             email (str): The email of the user.
@@ -40,22 +49,10 @@ def _register_user(self, email: str, password: str) -> Union[None, User]:
         Raises:
             ValueError: If a user with the given email already exists.
         """
-    try:
-        self.db.find_user_by(email=email)
-        raise ValueError(f"User with {email} alreay exists")
-    except NoResultFound:
-        hashed_password = _hash_password(password)
-        user = User(email=email, hashed_password=hashed_password)
-        self._db.add_user(user)
-        return user
-    pass
-
-
-class Auth:
-    """Auth class to interact with the authentication database.
-    """
-
-    def __init__(self):
-        """_summary_
-        """
-        self._db = DB()
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User with {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
